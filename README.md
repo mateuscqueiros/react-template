c4ebe1ba04797418df154e6b17a4ef667a489bdd
Mateus Queirós <mateuscqueiros@gmail.com>
2024-08-07 18:48:49 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# Template para sistemas Hadsys
+
+
+## Bibliotecas/pacotes
+
+Cache e camada da API: [Tanstack React Query](https://tanstack.com/query/latest)
---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from '@/components/ui/toggle-theme'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    return <ModeToggle />
+}
+
+export default App
